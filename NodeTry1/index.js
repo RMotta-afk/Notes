37ffbd82166832a64187a782689d9731b99b8fab
Raphael Motta <94933819+RMotta-afk@users.noreply.github.com>
2023-01-09 23:08:36 -0300

    Add files via upload

---
+++ NodeTry1/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+const { read } = require('fs');
+const path = require('path');
+
+async function readdir(rootDir){
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir);
+    walk(files);
+}
+
+function walk(files){
+    for (let file of files){
+        console.log(file);
+    }
+}
+
+readdir('./npmClass1');
